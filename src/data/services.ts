581434eca9db0a71bb8e1e13abe37724f55d3f85
Mika4vondenen <moneyprintercrp@gmail.com>
2025-12-07 19:00:44 +0100

    Updated services.ts

--- src/data/services.ts
+++ src/data/services.ts
@@ -1,7 +1,7 @@
 import eventPhoto from '../../assets/SON03067.webp';
 import eventVideoPhoto from '../../assets/20230813-SON08196-Verbessert-RR.webp';
 import eventEditingPhoto from '../../assets/20230701-SON06592.webp';
-import businessPhotoPhoto from '../../assets/20230701-SON06592.webp';
+import businessPhotoPhoto from '../../assets/e94e52c6c69a93e4b17a2570bb1a9e20.jpg';
 
 export interface Service {
   id: string;
