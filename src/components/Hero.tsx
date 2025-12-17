e1ae89ba4b9064f4b3559aad8140c92654960469
Mika4vondenen <moneyprintercrp@gmail.com>
2025-12-17 14:42:17 +0100

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ import { useAnimation } from '../context/AnimationContext';
 import heroImg1 from '../../assets/20231030-son05544-verbessert-rr.jpg';
 import heroImg2 from '../../assets/20250907-msk09631-verbessert-rr-2.jpg';
 import heroImg3 from '../../assets/20250907-msk09631-verbessert-rr-2.jpg';
-import heroImg4 from '../../assets/dslr_camera_lens_original_669131 copy.jpg';
+import heroImg4 from '../../assets/e94e52c6c69a93e4b17a2570bb1a9e20.jpg';
 
 export default function Hero() {
   const navigate = useNavigate();
