bf88e785608be88f4c3e843f432ffda74bd77cce
Mika4vondenen <moneyprintercrp@gmail.com>
2026-01-22 16:00:02 +0100

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,10 +6,11 @@ import { useAnimation } from '../context/AnimationContext';
 import heroImg1 from '../../assets/20231030-son05544-verbessert-rr.jpg';
 import heroImg2 from '../../assets/20250907-msk09631-verbessert-rr-2.jpg';
 import heroImg3 from '../../assets/20250907-msk09631-verbessert-rr-2.jpg';
+import heroImg4 from '../../assets/dslr_camera_lens_original_669131 copy.jpg';
 
 export default function Hero() {
   const navigate = useNavigate();
-  const images = [heroImg1, heroImg2, heroImg3];
+  const images = [heroImg1, heroImg2, heroImg3, heroImg4];
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const { showInitialAnimation } = useAnimation();
